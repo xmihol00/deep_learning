e44302a2e36a7df2b7bb14d953f8bf42ff0b7e72
David Mihola <mihola.dejv@gmail.com>
2022-12-07 17:53:00 +0100

    main-results all layers

--- run_training.sh
+++ run_training.sh
@@ -1,6 +1,3 @@
+python dropout_train_04.py > dropout_04.txt; python dropout_train_03.py > dropout_03.txt; python l1_train_00001.py > l1_00001.txt; python l1_train_0001.py > l1_0001.txt; python l1_train_001.py > l1_001.txt; python l2_train_0001.py > l2_0001.txt; python l2_train_001.py > l2_001.txt; python l1l2_train_0001.py > l1l2_0001.txt; python l1l2_train_001.py > l1l2_001.txt
 
-python basic_train.py > basic.txt
-python dropout_train.py > dropout.txt
-python l1_train.py > l1.txt
-python l2_train.py > l2.txt
-python l1l2_train.py > l1l2.txt
+
