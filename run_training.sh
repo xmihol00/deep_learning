bf8a134abb89242eba51a350800f6a87a2c53f40
David Mihola <david.mihola@sperling.cz>
2023-02-17 12:48:00 +0100

    main-cleanup of unnecessary files

--- run_training.sh
+++
@@ -1,3 +0,0 @@
-python dropout_train_04.py > dropout_04.txt; python dropout_train_03.py > dropout_03.txt; python l1_train_00001.py > l1_00001.txt; python l1_train_0001.py > l1_0001.txt; python l1_train_001.py > l1_001.txt; python l2_train_0001.py > l2_0001.txt; python l2_train_001.py > l2_001.txt; python l1l2_train_0001.py > l1l2_0001.txt; python l1l2_train_001.py > l1l2_001.txt
-
-
